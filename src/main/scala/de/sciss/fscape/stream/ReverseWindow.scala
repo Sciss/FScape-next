9f35d046fb9552f54aa2e9a0bba47800d494e823
Hanns Holger Rutz <contact@sciss.de>
2016-05-19 15:31:12 +0200

    add clump parameter to reverse-window

--- src/main/scala/de/sciss/fscape/stream/ReverseWindow.scala
+++ src/main/scala/de/sciss/fscape/stream/ReverseWindow.scala
@@ -16,43 +16,52 @@ package de.sciss.fscape.stream
 import akka.NotUsed
 import akka.stream.scaladsl.GraphDSL
 import akka.stream.stage.{GraphStage, GraphStageLogic}
-import akka.stream.{Attributes, FanInShape2, Inlet, Outlet}
+import akka.stream.{Attributes, FanInShape3, Inlet, Outlet}
 import de.sciss.fscape.Util
-import de.sciss.fscape.stream.impl.{FilterIn2Impl, WindowedLogicImpl}
+import de.sciss.fscape.stream.impl.{FilterIn3Impl, WindowedLogicImpl}
 
 /** Reverses contents of windowed input. */
 object ReverseWindow {
   /**
     * @param in     the signal to window
     * @param size   the window size. this is clipped to be `&lt;= 1`
+    * @param clump  clump size within each window. With a clump size of one,
+    *               each window is reversed sample by sample, if the clump size
+    *               is two, the first two samples are flipped with the last
+    *               two samples, then the third and forth are flipped with the
+    *               third and forth before last, etc. Like `size`, `clump` is
+    *               sampled at each beginning of a new window and held constant
+    *               during the window.
     */
-  def apply(in: Outlet[BufD], size: Outlet[BufI])
+  def apply(in: Outlet[BufD], size: Outlet[BufI], clump: Outlet[BufI])
            (implicit b: GraphDSL.Builder[NotUsed], ctrl: Control): Outlet[BufD] = {
     val stage0  = new Stage(ctrl)
     val stage   = b.add(stage0)
     import GraphDSL.Implicits._
-    in   ~> stage.in0
-    size ~> stage.in1
+    in    ~> stage.in0
+    size  ~> stage.in1
+    clump ~> stage.in2
 
     stage.out
   }
 
-  private final class Stage(ctrl: Control) extends GraphStage[FanInShape2[BufD, BufI, BufD]] {
-    val shape = new FanInShape2(
-      in0 = Inlet [BufD]("ReverseWindow.in"  ),
-      in1 = Inlet [BufI]("ReverseWindow.size"),
-      out = Outlet[BufD]("ReverseWindow.out" )
+  private final class Stage(ctrl: Control) extends GraphStage[FanInShape3[BufD, BufI, BufI, BufD]] {
+    val shape = new FanInShape3(
+      in0 = Inlet [BufD]("ReverseWindow.in"   ),
+      in1 = Inlet [BufI]("ReverseWindow.size" ),
+      in2 = Inlet [BufI]("ReverseWindow.clump"),
+      out = Outlet[BufD]("ReverseWindow.out"  )
     )
 
     def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new Logic(shape, ctrl)
   }
 
   // XXX TODO -- abstract over data type (BufD vs BufI)?
-  private final class Logic(protected val shape: FanInShape2[BufD, BufI, BufD],
+  private final class Logic(protected val shape: FanInShape3[BufD, BufI, BufI, BufD],
                             protected val ctrl: Control)
     extends GraphStageLogic(shape)
-      with WindowedLogicImpl[BufD, BufD, FanInShape2[BufD, BufI, BufD]]
-      with FilterIn2Impl                            [BufD, BufI, BufD] {
+      with WindowedLogicImpl[BufD, BufD, FanInShape3[BufD, BufI, BufI, BufD]]
+      with FilterIn3Impl                            [BufD, BufI, BufI, BufD] {
 
     protected val in0: Inlet[BufD] = shape.in0
 
@@ -60,11 +69,15 @@ object ReverseWindow {
 
     private[this] var winBuf : Array[Double] = _
     private[this] var winSize: Int = _
+    private[this] var clump  : Int = _
 
     protected def startNextWindow(inOff: Int): Int = {
       val oldSize = winSize
       if (bufIn1 != null && inOff < bufIn1.size) {
         winSize = math.max(1, bufIn1.buf(inOff))
+      }
+      if (bufIn2 != null && inOff < bufIn2.size) {
+        clump = math.max(1, bufIn2.buf(inOff))
       }
       if (winSize != oldSize) {
         winBuf = new Array[Double](winSize)
@@ -79,15 +92,21 @@ object ReverseWindow {
       Util.copy(winBuf, readFromWinOff, bufOut.buf, outOff, chunk)
 
     protected def processWindow(writeToWinOff: Int): Int = {
-      var i = 0
-      var j = writeToWinOff - 1
-      val b = winBuf
+      var i   = 0
+      val cl  = clump
+      val cl2 = cl + cl
+      var j   = writeToWinOff - cl
+      val b   = winBuf
       while (i < j) {
-        val tmp = b(i)
-        b(i) = b(j)
-        b(j) = tmp
-        i += 1
-        j -= 1
+        val k = i + cl
+        while (i < k) {
+          val tmp = b(i)
+          b(i) = b(j)
+          b(j) = tmp
+          i += 1
+          j += 1
+        }
+        j -= cl2
       }
       writeToWinOff
     }
