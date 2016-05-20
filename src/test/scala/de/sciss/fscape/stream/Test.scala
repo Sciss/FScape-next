2beec1c8dab89e3c4efe188cfb3615b5daf8046d
Hanns Holger Rutz <contact@sciss.de>
2016-05-20 14:49:11 +0200

    d'oh --- the `Broadcast` is wrong of course, as it doesn't allocate the buffer again

--- src/test/scala/de/sciss/fscape/stream/Test.scala
+++ src/test/scala/de/sciss/fscape/stream/Test.scala
@@ -2,7 +2,7 @@ package de.sciss.fscape.stream
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Source}
+import akka.stream.scaladsl.{Broadcast, GraphDSL, RunnableGraph, Sink, Source}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings, ClosedShape, Outlet, OverflowStrategy}
 import de.sciss.file._
 import de.sciss.fscape.gui.SimpleGUI
@@ -78,71 +78,101 @@ object Test extends App {
 //    ClosedShape
 //  }
 
-//  val graph = GraphDSL.create() { implicit b =>
-//    // 'analysis'
-//    val in          = DiskIn(file = fIn)
-//    val fftSize     = 131072
-//    val winStep     = fftSize / 4
-//    val inW         = Sliding       (in = in  , size = const(fftSize), step    = const(winStep))
-//    val fft         = Real1FullFFT  (in = inW , size = const(fftSize), padding = const(0))
-//
-//    // 'percussion'
-//    val log         = ComplexUnaryOp(in = fft , op = ComplexUnaryOp.Log)
-//    val logC        = BinaryOp      (a  = log , b = const(-80), op = BinaryOp.Max)
-//    val cep         = Complex1IFFT  (in = logC, size = const(fftSize), padding = const(0))
-//    val (pos0, neg) = UnzipWindow   (in = cep , size = const(fftSize))
-//    import GraphDSL.Implicits._
-//    val pos         = pos0.buffer(size = fftSize/blockSize, overflowStrategy = OverflowStrategy.backpressure).outlet
-//    val negR        = ReverseWindow (in = neg , size = const(fftSize), clump   = const(2))
-//
-//    /*
-//
-//    positive window: A = Re, B = im
-//    negative window: C = Re, D = im  (reversed order, i.e. from low to high frequencies)
-//
-//    A' = A * crr + C * ccr
-//    C' = C * clr + A * car
-//    B' = B * cri + D * cci
-//    D' = D * cli + B * cai
-//
-//     */
-//
-//    val (aIn, bIn)  = UnzipWindow   (in = pos , size = const(1))
-//    val (cIn, dIn)  = UnzipWindow   (in = negR, size = const(1))
-//
+  val graph = GraphDSL.create() { implicit b =>
+    // 'analysis'
+    val in          = DiskIn(file = fIn)
+    val fftSize     = 131072
+    val winStep     = fftSize / 4
+    val inW         = Sliding       (in = in  , size = const(fftSize), step    = const(winStep))
+    val fft         = Real1FullFFT  (in = inW , size = const(fftSize), padding = const(0))
+
+    // 'percussion'
+    val log         = ComplexUnaryOp(in = fft , op = ComplexUnaryOp.Log)
+    val logC        = BinaryOp      (a  = log , b = const(-80), op = BinaryOp.Max)
+    val cep         = Complex1IFFT  (in = logC, size = const(fftSize), padding = const(0))
+    val (pos0, neg) = UnzipWindow   (in = cep , size = const(fftSize))
+    import GraphDSL.Implicits._
+    val pos         = pos0.buffer(size = fftSize/blockSize, overflowStrategy = OverflowStrategy.backpressure).outlet
+    val negR        = ReverseWindow (in = neg , size = const(fftSize), clump   = const(2))
+
+    /*
+
+    positive window: A = Re, B = im
+    negative window: C = Re, D = im  (reversed order, i.e. from low to high frequencies)
+
+    A' = A * crr + C * ccr
+    C' = C * clr + A * car
+    B' = B * cri + D * cci
+    D' = D * cli + B * cai
+
+     */
+
+    val (aIn, bIn)  = UnzipWindow   (in = pos , size = const(1))
+    val (cIn, dIn)  = UnzipWindow   (in = negR, size = const(1))
+
 //    val crr =  0; val cri =  0
 //    val clr = +1; val cli = +1
 //    val ccr = +1; val cci = -1
 //    val car = +1; val cai = -1
-//
-//    val am1         = BinaryOp(op = BinaryOp.Times, a = aIn, b = const(crr))
-//    val am2         = BinaryOp(op = BinaryOp.Times, a = cIn, b = const(ccr))
-//    val aOut        = BinaryOp(op = BinaryOp.Plus , a = am1, b = am2)
-//
-//    val bm1         = BinaryOp(op = BinaryOp.Times, a = bIn, b = const(cri))
-//    val bm2         = BinaryOp(op = BinaryOp.Times, a = dIn, b = const(cci))
-//    val bOut        = BinaryOp(op = BinaryOp.Plus , a = bm1, b = bm2)
-//
-//    val cm1         = BinaryOp(op = BinaryOp.Times, a = cIn, b = const(clr))
-//    val cm2         = BinaryOp(op = BinaryOp.Times, a = aIn, b = const(car))
-//    val cOut        = BinaryOp(op = BinaryOp.Plus , a = cm1, b = cm2)
-//
-//    val dm1         = BinaryOp(op = BinaryOp.Times, a = dIn, b = const(cli))
-//    val dm2         = BinaryOp(op = BinaryOp.Times, a = bIn, b = const(cai))
-//    val dOut        = BinaryOp(op = BinaryOp.Plus , a = dm1, b = dm2)
-//
-//    val posOut      = ??? : Outlet[BufD]  // ZipWindow(size = const(1))
-//    val negOutR     = ??? : Outlet[BufD]  // ZipWindow(size = const(1))
-//    val negOut      = ReverseWindow (in = negOutR, size = const(fftSize), clump = const(2))
-//    val logOut      = ??? : Outlet[BufD]  // ZipWindow(size = const(fftSize)) -- input buffer!
-//    val fftOut      = ComplexUnaryOp(in = logOut, op = ComplexUnaryOp.Exp)
-//    val outW        = Real1FullIFFT(in = fftOut, size = const(fftSize), padding = const(0))
+
+    val crr = +1; val cri = +1
+    val clr = +1; val cli = +1
+    val ccr =  0; val cci =  0
+    val car =  0; val cai =  0
+
+    val aInB        = b add Broadcast[BufD](2)
+    aIn ~> aInB.in
+    val bInB        = b add Broadcast[BufD](2)
+    bIn ~> bInB.in
+    val cInB        = b add Broadcast[BufD](2)
+    cIn ~> cInB.in
+    val dInB        = b add Broadcast[BufD](2)
+    dIn ~> dInB.in
+
+    val am1         = BinaryOp(op = BinaryOp.Times, a = aInB.out(0), b = const(crr))
+    // val am2         = BinaryOp(op = BinaryOp.Times, a = cInB.out(0), b = const(ccr))
+    val aOut        = BinaryOp(op = BinaryOp.Plus , a = am1, b = const(0) /* am2 */)
+
+    val bm1         = BinaryOp(op = BinaryOp.Times, a = bInB.out(0), b = const(cri))
+    // val bm2         = BinaryOp(op = BinaryOp.Times, a = dInB.out(0), b = const(cci))
+    val bOut        = BinaryOp(op = BinaryOp.Plus , a = bm1, b = const(0) /* bm2 */)
+
+    val cm1         = BinaryOp(op = BinaryOp.Times, a = cInB.out(1), b = const(clr))
+    // val cm2         = BinaryOp(op = BinaryOp.Times, a = aInB.out(1), b = const(car))
+    val cOut        = BinaryOp(op = BinaryOp.Plus , a = cm1, b = const(0) /* cm2 */)
+
+    val dm1         = BinaryOp(op = BinaryOp.Times, a = dInB.out(1), b = const(cli))
+    // val dm2         = BinaryOp(op = BinaryOp.Times, a = bInB.out(1), b = const(cai))
+    val dOut        = BinaryOp(op = BinaryOp.Plus , a = dm1, b = const(0) /* dm2 */)
+
+//    am2 ~> Sink.ignore
+//    bm2 ~> Sink.ignore
+//    cm2 ~> Sink.ignore
+//    dm2 ~> Sink.ignore
+
+    aInB.out(1) ~> Sink.ignore
+    bInB.out(1) ~> Sink.ignore
+    cInB.out(0) ~> Sink.ignore
+    dInB.out(0) ~> Sink.ignore
+
+//    val aOut = BinaryOp(op = BinaryOp.Times, a = aIn, b = const(crr))
+//    val bOut = BinaryOp(op = BinaryOp.Times, a = bIn, b = const(cri))
+//    val cOut = BinaryOp(op = BinaryOp.Times, a = cIn, b = const(clr))
+//    val dOut = BinaryOp(op = BinaryOp.Times, a = dIn, b = const(cli))
+
+    val posOut      = ZipWindow(a = aOut, b = bOut, size = const(1))
+    val negOutR     = ZipWindow(a = cOut, b = dOut, size = const(1))
+    val negOut      = ReverseWindow (in = negOutR, size = const(fftSize), clump = const(2))
+    val logOut      = ZipWindow(a = posOut, b = negOut, size = const(fftSize))
+    val freq        = Complex1FFT   (in = logOut, size = const(fftSize), padding = const(0))
+    val fftOut      = ComplexUnaryOp(in = freq, op = ComplexUnaryOp.Exp)
+    val outW        = Real1FullIFFT(in = fftOut, size = const(fftSize), padding = const(0))
 //
 //    // 'synthesis'
-//    val sig         = outW  // XXX TODO: apply window function and overlap-add
-//    DiskOut(file = fOut, spec = AudioFileSpec(numChannels = 1, sampleRate = 44100), in = sig)
-//    ClosedShape
-//  }
+    val sig         = outW  // XXX TODO: apply window function and overlap-add
+    DiskOut(file = fOut, spec = AudioFileSpec(numChannels = 1, sampleRate = 44100), in = sig)
+    ClosedShape
+  }
 
 //  val graph = GraphDSL.create() { implicit b =>
 //    val in      = DiskIn(file = fIn)
