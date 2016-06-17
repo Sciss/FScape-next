435f73b75ac522066829889d77654f2ec4b31b8e
Hanns Holger Rutz <contact@sciss.de>
2016-06-17 18:00:22 +0200

    elastic - index check is wrong

--- src/main/scala/de/sciss/fscape/stream/Elastic.scala
+++ src/main/scala/de/sciss/fscape/stream/Elastic.scala
@@ -52,68 +52,83 @@ object Elastic {
     private[this] var num     : Int         = _
     private[this] var buffers : Array[BufD] = _
 
-    private[this] var bufReadIdx  = 0   // circular
-    private[this] var bufWriteIdx = 0   // circular
+    private[this] var bufPopIdx   = 0   // circular
+    private[this] var bufPushIdx  = 0   // circular
 
     @inline
-    private def canPushBuf  = bufReadIdx != bufWriteIdx
+    private def canUseBuf  = bufPopIdx != bufPushIdx
 
     @inline
-    private def canPopBuf   = bufReadIdx != bufWriteIdx
+    private def shouldRead  = canRead  && (canUseBuf  || bufIn0 == null)
 
     @inline
-    private def shouldRead  = canRead  && (canPushBuf || bufIn0 == null)
-
-    @inline
-    private def shouldWrite = canWrite && (canPopBuf  || bufIn0 != null)
+    private def shouldWrite = canWrite && (canUseBuf  || bufIn0 != null)
 
     override def postStop(): Unit = {
       super.postStop()
-      while (bufReadIdx < bufWriteIdx) {
-        buffers(bufReadIdx).release()
-        buffers(bufReadIdx) = null
-        bufReadIdx = (bufReadIdx + 1) % num
+      println("POST STOP")
+      while (bufPopIdx < bufPushIdx) {
+        buffers(bufPopIdx).release()
+        buffers(bufPopIdx) = null
+        bufPopIdx = (bufPopIdx + 1) % num
       }
+    }
+
+    def logStreamX(what: => String): Unit = {
+      println(what)
+    }
+
+    private def pushBuffer(): Unit = {
+      println(s"push buffers($bufPushIdx) = $bufIn0")
+      assert(bufIn0 != null)
+      buffers(bufPushIdx) = bufIn0
+      bufIn0 = null
+      bufPushIdx = (bufPushIdx + 1) % num
+      println(s"bufPopIdx = $bufPopIdx; bufPushIdx = $bufPushIdx")
     }
 
     @tailrec
     def process(): Unit = {
-      logStream(s"process() $this")
+      logStreamX(s"process() $this")
+      logStreamX(s"-- canRead? $canRead; canWrite? $canWrite; bufIn0 ${bufIn0 != null}; readIdx $bufPopIdx; writeIdx $bufPushIdx")
       var stateChange = false
 
       if (shouldRead) {
         readIns()
+        assert(bufIn0 != null)
+
         if (init) {
           num     = math.max(0, bufIn1.buf(0))
           buffers = new Array[BufD](num)
+          if (num > 0) pushBuffer()
           init    = false
-        }
-        if (canPushBuf) {
-          buffers(bufWriteIdx) = bufIn0
-          bufIn0 = null
-          bufWriteIdx = (bufWriteIdx + 1) % num
+        } else if (canUseBuf) {
+          pushBuffer()
         }
         stateChange = true
       }
 
       if (shouldWrite) {
-        val buf = if (canPopBuf) {
-          val res = buffers(bufReadIdx)
-          buffers(bufReadIdx) = null
-          bufReadIdx = (bufReadIdx + 1) % num
+        val buf = if (canUseBuf) {
+          val res = buffers(bufPopIdx)
+          println(s"read buffers($bufPopIdx) = $res; -> set null")
+          buffers(bufPopIdx) = null
+          bufPopIdx = (bufPopIdx + 1) % num
+          println(s"bufPopIdx = $bufPopIdx; bufPushIdx = $bufPushIdx")
           res
         } else {
           val res = bufIn0
           bufIn0 = null
           res
         }
+        assert(buf != null)
         push(out0, buf)
         updateCanWrite()
         stateChange = true
       }
 
-      if (isClosed(in0) && !canPopBuf && bufIn0 == null) {
-        logStream(s"completeStage() $this")
+      if (isClosed(in0) && !canUseBuf && bufIn0 == null) {
+        logStreamX(s"completeStage() $this")
         completeStage()
       }
       else if (stateChange) process()
--- src/test/scala/de/sciss/fscape/ZipWindowTest.scala
+++ src/test/scala/de/sciss/fscape/ZipWindowTest.scala
@@ -70,7 +70,7 @@ object ZipWindowTest extends App {
 
     val fftAZ0 = UnzipWindow(fftA) // treat Re and Im as two channels
     // val fftBZ = UnzipWindow(fftB) // treat Re and Im as two channels
-    val fftAZ = fftAZ0.elastic(2048)
+    val fftAZ = fftAZ0.elastic(1024)
 //    val fftAZ = BufferDisk(fftAZ0)
 
     val numFrames = math.min(fftSizeA, fftSizeB)
@@ -79,7 +79,7 @@ object ZipWindowTest extends App {
     // fftBZ.poll(1.0/44100, "B")
   }
 
-  showStreamLog = true
+  // showStreamLog = true
 
   val config = stream.Control.Config()
   config.useAsync = false // for debugging
