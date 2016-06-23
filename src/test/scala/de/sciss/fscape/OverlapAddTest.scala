028e4ffe21383bf4bc0cd7fc06ccb44c2b51fda6
Hanns Holger Rutz <contact@sciss.de>
2016-06-23 17:39:41 +0200

    the culprit seems to be `elastic`

--- src/test/scala/de/sciss/fscape/OverlapAddTest.scala
+++ src/test/scala/de/sciss/fscape/OverlapAddTest.scala
@@ -44,12 +44,14 @@ object OverlapAddTest extends App {
     val lap           = OverlapAdd(in = windowed, size = inputWinSize, step = stepSize /* + shiftXPad */)
     val drop          = lap.drop(numPadLeft).take(numFrames)
     val sig0          = drop * gain
-    val disk1         = disk.elastic((numPadLeft + config.blockSize - 1) / config.blockSize)
+//    val disk1         = DiskIn(file = in, numChannels = 1)
+//    val disk1         = disk.elastic((numPadLeft + config.blockSize - 1) / config.blockSize)
+    val disk1         = BufferDisk(disk)
     sig0 .poll(1.0/44100, "out")
     disk1.poll(1.0/44100, "in ")
     // es ist zum Heulen -- mit der folgenden Zeile werden die Buffer wieder korrupt
-    // val sig           = sig0 - disk1
-    val sig           = sig0
+     val sig           = sig0 - disk1
+//    val sig           = sig0
 //    val sig = slide
     DiskOut(file = out, spec = AudioFileSpec(sampleRate = 44100.0, numChannels = 1), in = sig)
   }
