bc4bf4960f2c0ad215d17c03e9b7b4f0e5015c0e
Hanns Holger Rutz <contact@sciss.de>
2016-06-23 17:13:35 +0200

    me parece bien

--- src/test/scala/de/sciss/fscape/OverlapAddTest.scala
+++ src/test/scala/de/sciss/fscape/OverlapAddTest.scala
@@ -17,26 +17,27 @@ object OverlapAddTest extends App {
 
   lazy val g = Graph {
     import graph._
-    // val disk          = DiskIn(file = in, numChannels = 1)
+   val disk          = DiskIn(file = in, numChannels = 1)
     // val disk = SinOsc(10.0/44100).take(44100)
     // val disk = DC(0.5).take(2000)
-    val disk = SinOsc(1.0/200).take(3000) // ++ DC(0.0).take(3000)
+//    val disk = SinOsc(1.0/200).take(3000) // ++ DC(0.0).take(3000)
 //    // val disk1         = DiskIn(file = in, numChannels = 1)
     val stepSize      = 100
-    val inputWinSize  = stepSize * 4 // 16384
+    val overlap       = 4
+    val inputWinSize  = stepSize * overlap // 16384
     val win           = GenWindow(size = inputWinSize, shape = GenWindow.Hann)
     val gain          = 0.5
-//    val numPadLeft    = inputWinSize - stepSize
-//    val padLeft       = DC(0.0).take(numPadLeft)
-    val slideIn       = disk // spadLeft ++ disk
+    val numPadLeft    = inputWinSize - stepSize
+    val padLeft       = DC(0.0).take(numPadLeft)
+    val slideIn       = padLeft ++ disk
     val slide0        = Sliding   (in = slideIn , size = inputWinSize, step = stepSize)
-    val slide = slide0.take(12000)
+    val slide         = slide0 // .drop(numPadLeft * overlap) // take(12000)
     // val shiftXPad     = 0: GE
     val windowed      = slide * win
 //     val windowed = DC(0.125).take(12000)
 //    val windowed = SinOsc(0.25).take(12000) * 0.25
     val lap           = OverlapAdd(in = windowed, size = inputWinSize, step = stepSize /* + shiftXPad */)
-    val drop          = lap // .drop(numPadLeft)
+    val drop          = lap.drop(numPadLeft)
     val sig0          = drop * gain
     val sig           = sig0 // - disk1
 //    val sig = slide
@@ -46,7 +47,7 @@ object OverlapAddTest extends App {
   // showStreamLog = true
 
   val config = stream.Control.Config()
-  config.blockSize = 1020
+  config.blockSize = 1024
   config.useAsync = false // for debugging
   val ctrl = stream.Control(config)
   ctrl.run(g)
