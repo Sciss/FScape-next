6ad95124690c080f1b5c86d49d6d837c25889b06
Hanns Holger Rutz <contact@sciss.de>
2016-07-30 23:27:18 +0200

    ok, so here's a simple test case with step > winSize for sliding

---
+++ src/test/scala/de/sciss/fscape/SlidingTest.scala
@@ -0,0 +1,29 @@
+package de.sciss.fscape
+
+import de.sciss.fscape.gui.SimpleGUI
+
+import scala.swing.Swing
+
+object SlidingTest extends App {
+  val config = stream.Control.Config()
+  config.blockSize = 1024
+  config.useAsync = false // for debugging
+  val ctrl = stream.Control(config)
+
+  lazy val g = Graph {
+    import graph._
+    val sig     = WhiteNoise()
+    val sliding = Sliding(sig, size = 4, step = 16)
+    sliding.take(2000).poll(1.0/500, "foo")
+  }
+
+  // showStreamLog = true
+
+  ctrl.run(g)
+
+  Swing.onEDT {
+    SimpleGUI(ctrl)
+  }
+
+  println("Running.")
+}
