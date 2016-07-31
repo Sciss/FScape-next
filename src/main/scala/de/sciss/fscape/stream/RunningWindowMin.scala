410a73ffc00537a7f99d85d1ae8cfef9a7b07eee
Hanns Holger Rutz <contact@sciss.de>
2016-07-31 21:48:48 +0200

    quick sketch of RunningWindowValue -- not tested yet

---
+++ src/main/scala/de/sciss/fscape/stream/RunningWindowMin.scala
@@ -0,0 +1,55 @@
+/*
+ *  RunningWindowMin.scala
+ *  (FScape)
+ *
+ *  Copyright (c) 2001-2016 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is published under the GNU General Public License v2+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.fscape
+package stream
+
+import akka.stream.stage.GraphStageLogic
+import akka.stream.{Attributes, FanInShape3}
+import de.sciss.fscape.stream.impl.{FilterIn3DImpl, RunningWindowValueImpl, StageImpl, StageLogicImpl}
+
+object RunningWindowMin {
+  def apply(in: OutD, size: OutI, trig: OutI)(implicit b: Builder): OutD = {
+    val stage0  = new Stage
+    val stage   = b.add(stage0)
+    b.connect(in  , stage.in0)
+    b.connect(size, stage.in1)
+    b.connect(trig, stage.in2)
+    stage.out
+  }
+
+  private final val name = "RunningWindowMin"
+
+  private type Shape = FanInShape3[BufD, BufI, BufI, BufD]
+
+  private final class Stage(implicit ctrl: Control) extends StageImpl[Shape](name) {
+    val shape = new FanInShape3(
+      in0 = InD (s"$name.in"  ),
+      in1 = InI (s"$name.size"),
+      in2 = InI (s"$name.trig"),
+      out = OutD(s"$name.out" )
+    )
+
+    def createLogic(attr: Attributes): GraphStageLogic = new Logic(shape)
+  }
+
+  private final class Logic(shape: Shape)(implicit ctrl: Control)
+    extends StageLogicImpl(name, shape)
+      with RunningWindowValueImpl[Shape]
+      with FilterIn3DImpl[BufD, BufI, BufI] {
+
+//    protected def neutralValue: Double = Double.PositiveInfinity
+
+    protected def combine(a: Double, b: Double): Double = math.min(a, b)
+  }
+}
---
+++ src/main/scala/de/sciss/fscape/stream/impl/RunningWindowValueImpl.scala
@@ -0,0 +1,83 @@
+/*
+ *  RunningWindowValueImpl.scala
+ *  (FScape)
+ *
+ *  Copyright (c) 2001-2016 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is published under the GNU General Public License v2+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.fscape
+package stream
+package impl
+
+import akka.stream.Shape
+import akka.stream.stage.GraphStageLogic
+
+trait RunningWindowValueImpl[S <: Shape]
+  extends FilterLogicImpl[BufD, S]
+  with WindowedLogicImpl[S] {
+  _: GraphStageLogic =>
+
+  // ---- abstract ----
+
+  protected def combine(a: Double, b: Double): Double
+
+  protected def bufIn0 : BufD
+  protected def bufIn1 : BufI
+  protected def bufIn2 : BufI
+  protected def bufOut0: BufD
+
+  // ---- impl ----
+
+//  private[this] var value = neutralValue
+  private[this] var trig0 = false
+
+  private[this] var winSize: Int = _
+  private[this] var winBuf : Array[Double] = _
+
+  protected final def startNextWindow(inOff: Int): Int = {
+    val oldSize = winSize
+    if (bufIn1 != null && inOff < bufIn1.size) {
+      winSize = math.max(1, bufIn1.buf(inOff))
+    }
+    if (winSize != oldSize) {
+      winBuf = new Array[Double](winSize)
+      trig0 = true
+    } else {
+      trig0 = false
+    }
+    winSize
+  }
+
+  protected def copyWindowToOutput(readFromWinOff: Int, outOff: Int, chunk: Int): Unit =
+    Util.copy(winBuf, readFromWinOff, bufOut0.buf, outOff, chunk)
+
+  protected final def processWindow(writeToWinOff: Int): Int = writeToWinOff
+
+  // XXX TODO --- this is very similar to processChunk in RunningValueImpl; perhaps DRY?
+  protected final def copyInputToWindow(inOff: Int, writeToWinOff: Int, chunk: Int): Unit = {
+    // Util.copy(bufIn0.buf, inOff, winBuf, writeToWinOff, chunk)
+    var inOffI  = inOff
+    var outOffI = writeToWinOff
+    val stop0   = inOffI + chunk
+    val b0      = bufIn0.buf
+    val b2      = if (bufIn2 == null) null else bufIn2.buf
+    val out     = winBuf
+    val stop2   = if (b2 == null) 0 else bufIn2.size
+    var t0      = trig0
+    while (inOffI < stop0) {
+      val x0 = b0(inOffI)
+      if (!t0 && inOffI < stop2 && b2(inOffI) > 0) t0 = true
+      val v = if (t0) x0 else combine(out(outOffI), x0)
+      out(outOffI) = v
+      inOffI  += 1
+      outOffI += 1
+    }
+    trig0 = t0
+  }
+}
