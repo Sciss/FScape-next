789aacad44fa94d015ab6c639b8c6e9b5f296462
Hanns Holger Rutz <contact@sciss.de>
2016-08-18 00:31:11 +0200

    step

--- src/main/scala/de/sciss/fscape/stream/Resample.scala
+++ src/main/scala/de/sciss/fscape/stream/Resample.scala
@@ -14,9 +14,11 @@
 package de.sciss.fscape
 package stream
 
-import akka.stream.stage.GraphStageLogic
-import akka.stream.{Attributes, FanInShape6}
+import akka.stream.stage.{GraphStageLogic, InHandler}
+import akka.stream.{Attributes, FanInShape6, Inlet}
 import de.sciss.fscape.stream.impl.{ChunkImpl, FilterIn6DImpl, FilterLogicImpl, StageImpl, StageLogicImpl}
+
+import scala.annotation.tailrec
 
 object Resample {
   def apply(in: OutD, factor: OutD, minFactor: OutD, rollOff: OutD, kaiserBeta: OutD, zeroCrossings: OutI)
@@ -52,50 +54,190 @@ object Resample {
 
   private final class Logic(shape: Shape)(implicit ctrl: Control)
     extends StageLogicImpl(name, shape)
-      with ChunkImpl[Shape]
-      with FilterLogicImpl[BufD, Shape]
-      with FilterIn6DImpl[BufD, BufD, BufD, BufD, BufD, BufI] {
+//      with ChunkImpl[Shape]
+//      with FilterLogicImpl[BufD, Shape]
+//      with FilterIn6DImpl[BufD, BufD, BufD, BufD, BufD, BufI]
+  {
 
-    protected def shouldComplete(): Boolean = ???
-    
+    private[this] var factor        = -1.0
+    private[this] var minFactor     = -1.0
     private[this] var rollOff       = -1.0
     private[this] var kaiserBeta    = -1.0
     private[this] var zeroCrossings = -1
 
-    /** Should read and possibly update `inRemain`, `outRemain`, `inOff`, `outOff`.
-      *
-      * @return `true` if this method did any actual processing.
-      */
-    protected def processChunk(): Boolean = {
-      var newTable = false
+    private[this] var bufIn           : BufD = _
+    private[this] var bufFactor       : BufD = _
+    private[this] var bufMinFactor    : BufD = _
+    private[this] var bufRollOff      : BufD = _
+    private[this] var bufKaiserBeta   : BufD = _
+    private[this] var bufZeroCrossings: BufI = _
+    private[this] var bufOut          : BufD = _
 
-      val inOffI = inOff
+    private[this] var _inAuxValid = false
+    private[this] var _canReadAux = false
 
-      if (bufIn3 != null && inOffI < bufIn3.size) {
-        val newRollOff = math.max(0.0, math.min(1.0, bufIn3.buf(inOffI)))
-        if (rollOff != newRollOff) {
-          rollOff   = newRollOff
-          newTable  = true
+    override def preStart(): Unit = {
+      val sh = shape
+      pull(sh.in0)
+      pull(sh.in1)
+      pull(sh.in2)
+      pull(sh.in3)
+      pull(sh.in4)
+      pull(sh.in5)
+    }
+
+    override def postStop(): Unit = {
+      freeAuxInputBuffers()
+      freeOutputBuffers()
+    }
+
+    private def freeAuxInputBuffers(): Unit = {
+      if (bufFactor != null) {
+        bufFactor.release()
+        bufFactor = null
+      }
+      if (bufMinFactor != null) {
+        bufMinFactor.release()
+        bufMinFactor = null
+      }
+      if (bufRollOff != null) {
+        bufRollOff.release()
+        bufRollOff = null
+      }
+      if (bufKaiserBeta != null) {
+        bufKaiserBeta.release()
+        bufKaiserBeta = null
+      }
+      if (bufZeroCrossings != null) {
+        bufZeroCrossings.release()
+        bufZeroCrossings = null
+      }
+    }
+
+    private def freeOutputBuffers(): Unit =
+      if (bufOut != null) {
+        bufOut.release()
+        bufOut = null
+      }
+
+    private[this] val fltSmpPerCrossing = 4096
+
+    private def readAuxIns(): Int = {
+      freeAuxInputBuffers()
+      val sh  = shape
+      var res = 0
+
+      if (isAvailable(sh.in1)) {
+        bufFactor = grab(sh.in1)
+        tryPull(sh.in1)
+        res = bufFactor.size
+      }
+
+      if (isAvailable(sh.in2)) {
+        bufMinFactor = grab(sh.in2)
+        tryPull(sh.in2)
+        res = if (res == 0) bufMinFactor.size else math.min(res, bufMinFactor.size)
+      }
+
+      if (isAvailable(sh.in3)) {
+        bufRollOff = grab(sh.in3)
+        tryPull(sh.in3)
+        res = if (res == 0) bufRollOff.size else math.min(res, bufRollOff.size)
+      }
+
+      if (isAvailable(sh.in4)) {
+        bufKaiserBeta = grab(sh.in4)
+        tryPull(sh.in4)
+        res = if (res == 0) bufKaiserBeta.size else math.min(res, bufKaiserBeta.size)
+      }
+
+      if (isAvailable(sh.in5)) {
+        bufZeroCrossings = grab(sh.in5)
+        tryPull(sh.in5)
+        res = if (res == 0) bufZeroCrossings.size else math.min(res, bufZeroCrossings.size)
+      }
+
+//      if (!_inAuxValid) {
+//        // calculate buffer only once
+//        val fltLen = ((fltSmpPerCrossing * zeroCrossings) / rollOff + 0.5).toInt
+        _inAuxValid = true
+//      }
+
+      _canReadAux = false
+      res
+    }
+
+    private class AuxInHandler[A](in: Inlet[A])
+      extends InHandler {
+
+      def onPush(): Unit = {
+        logStream(s"onPush($in)")
+        testRead()
+      }
+
+      private[this] def testRead(): Unit = {
+        updateCanReadAux()
+        if (_canReadAux) process()
+      }
+
+      override def onUpstreamFinish(): Unit = {
+        logStream(s"onUpstreamFinish($in)")
+        if (_inAuxValid || isAvailable(in)) {
+          testRead()
+        } else {
+          println(s"Invalid aux $in")
+          completeStage()
         }
       }
 
-      if (bufIn4 != null && inOffI < bufIn4.size) {
-        val newKaiserBeta = math.max(0.0, bufIn4.buf(inOffI))
-        if (kaiserBeta != newKaiserBeta) {
-          kaiserBeta  = newKaiserBeta
-          newTable    = true
-        }
-      }
+      setHandler(in, this)
+    }
 
-      if (bufIn5 != null && inOffI < bufIn5.size) {
-        val newZeroCrossings = math.max(1, bufIn5.buf(inOffI))
-        if (zeroCrossings != newZeroCrossings) {
-          zeroCrossings = newZeroCrossings
-          newTable      = true
-        }
-      }
-      
+    new AuxInHandler(shape.in1)
+    new AuxInHandler(shape.in2)
+    new AuxInHandler(shape.in3)
+    new AuxInHandler(shape.in4)
+    new AuxInHandler(shape.in5)
+
+    // @tailrec
+    private[this] def process(): Unit = {
       ???
     }
+
+    private def updateCanReadAux(): Unit = {
+      val sh = shape
+      _canReadAux =
+        ((isClosed(sh.in1) && _inAuxValid) || isAvailable(sh.in1)) &&
+        ((isClosed(sh.in2) && _inAuxValid) || isAvailable(sh.in2)) &&
+        ((isClosed(sh.in3) && _inAuxValid) || isAvailable(sh.in3)) &&
+        ((isClosed(sh.in4) && _inAuxValid) || isAvailable(sh.in4)) &&
+        ((isClosed(sh.in5) && _inAuxValid) || isAvailable(sh.in5))
+    }
+
+    //      val inOffI = inOff
+//
+//      if (bufIn3 != null && inOffI < bufIn3.size) {
+//        val newRollOff = math.max(0.0, math.min(1.0, bufIn3.buf(inOffI)))
+//        if (rollOff != newRollOff) {
+//          rollOff   = newRollOff
+//          newTable  = true
+//        }
+//      }
+//
+//      if (bufIn4 != null && inOffI < bufIn4.size) {
+//        val newKaiserBeta = math.max(0.0, bufIn4.buf(inOffI))
+//        if (kaiserBeta != newKaiserBeta) {
+//          kaiserBeta  = newKaiserBeta
+//          newTable    = true
+//        }
+//      }
+//
+//      if (bufIn5 != null && inOffI < bufIn5.size) {
+//        val newZeroCrossings = math.max(1, bufIn5.buf(inOffI))
+//        if (zeroCrossings != newZeroCrossings) {
+//          zeroCrossings = newZeroCrossings
+//          newTable      = true
+//        }
+//      }
   }
 }
