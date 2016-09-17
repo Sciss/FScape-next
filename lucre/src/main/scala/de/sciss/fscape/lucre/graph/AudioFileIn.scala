ebf620b60056d1e255425a11d63f11179399822d
Hanns Holger Rutz <contact@sciss.de>
2016-09-17 14:26:34 +0200

    add audio-file-in > numFrames, sampleRate

--- lucre/src/main/scala/de/sciss/fscape/lucre/graph/AudioFileIn.scala
+++ lucre/src/main/scala/de/sciss/fscape/lucre/graph/AudioFileIn.scala
@@ -20,19 +20,47 @@ import de.sciss.fscape
 import de.sciss.synth.io.AudioFile
 import de.sciss.synth.proc.AudioCue
 
-final case class AudioFileIn(key: String) extends GE.Lazy {
-  protected def makeUGens(implicit b: UGenGraph.Builder): UGenInLike = {
+object AudioFileIn {
+  final case class NumFrames(key: String) extends GE.Lazy {
+    override def productPrefix = s"AudioFileIn$$NumFrames"
+
+    protected def makeUGens(implicit b: UGenGraph.Builder): UGenInLike = {
+      val cue = AudioFileIn.getCue(key, b)
+      cue.spec.numFrames - cue.fileOffset
+    }
+  }
+
+  final case class SampleRate(key: String) extends GE.Lazy {
+    override def productPrefix = s"AudioFileIn$$SampleRate"
+
+    protected def makeUGens(implicit b: UGenGraph.Builder): UGenInLike = {
+      val cue = AudioFileIn.getCue(key, b)
+      cue.spec.sampleRate
+    }
+  }
+
+  private def getCue(key: String, b: UGenGraph.Builder): AudioCue = {
     val ub  = UGenGraphBuilder.get(b)
-    val cue = ub.requestAttribute(key).fold[AudioCue](sys.error(s"Missing Attribute $key")) {
+    val res = ub.requestAttribute(key).fold[AudioCue](sys.error(s"AudioFileIn missing attribute $key")) {
       case a: AudioCue => a
       case f: File =>
         val spec = AudioFile.readSpec(f)
         AudioCue(f, spec, 0L, 1.0)
-      case other => sys.error(s"$this - requires AudioCue or Artifact value, found $other")
+      case other => sys.error(s"AudioFileIn - requires AudioCue or Artifact value, found $other")
     }
+    res
+  }
+}
+final case class AudioFileIn(key: String) extends GE.Lazy {
+  protected def makeUGens(implicit b: UGenGraph.Builder): UGenInLike = {
+    val cue = AudioFileIn.getCue(key, b)
     val in0 = fscape.graph.AudioFileIn(file = cue.artifact, numChannels = cue.spec.numChannels)
-    val in1 = if (cue.offset <= 0) in0 else in0.drop(cue.offset)
+    val off = cue.fileOffset
+    val in1 = if (off <= 0) in0 else in0.drop(off)
     val in  = if (cue.gain == 1.0) in1 else in1 * cue.gain
     in
   }
+
+  def numFrames : GE = AudioFileIn.SampleRate (key)
+  def sampleRate: GE = AudioFileIn.SampleRate(key)
 }
