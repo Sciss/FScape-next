0be1ca451c2e8a4a26f6029306b16a95d62e001e
Hanns Holger Rutz <contact@sciss.de>
2016-09-26 00:17:35 +0200

    ah well, no.

--- core/src/test/scala/de/sciss/fscape/MFCCTest.scala
+++ core/src/test/scala/de/sciss/fscape/MFCCTest.scala
@@ -19,17 +19,21 @@ object MFCCTest extends App {
 
   lazy val g0 = Graph {
     import graph._
-//    import specIn.{sampleRate, numChannels, numFrames}
-//    val in          = AudioFileIn(fIn, numChannels = numChannels)
-    val numChannels = 1
-    val numFrames   = 44100 * 8
-    val sampleRate  = 44100.0
-    val in          = SinOsc(441/sampleRate).take(44100 * 4) ++ SinOsc(882/sampleRate).take(44100 * 4)
+    import specIn.{sampleRate, numChannels, numFrames}
+    val in          = AudioFileIn(fIn, numChannels = numChannels)
+//    val numChannels = 1
+//    val numFrames   = 44100 * 8
+//    val sampleRate  = 44100.0
+//    val in          = SinOsc(441/sampleRate).take(44100 * 4) ++ SinOsc(882/sampleRate).take(44100 * 4)
 
     val fftSize     = 1024
     val stepSize    = fftSize / 2
     val numMel      = 42
     val numCoef     = 13
+    val sideLen     = 24 // 32
+    val numTop      = 10
+    val covSize     = numCoef * sideLen
+    val numCov      = numFrames / stepSize - (2 * sideLen)
 
     val lap       = Sliding (in , fftSize, stepSize) * GenWindow(fftSize, GenWindow.Hann)
     val fft       = Real1FFT(lap, fftSize, mode = 1)
@@ -39,20 +43,21 @@ object MFCCTest extends App {
     val mfcc      = DCT_II(mel.log, numMel, numCoef, zero = 0)
 
     // reconstruction of what strugatzki's 'segmentation' is doing (first step)
-    val covSize   = numCoef * 32
-    val mfccSlid  = Sliding(mfcc, numCoef, 1)
+//    val mfccSlid  = Sliding(mfcc, numCoef, 1)
+    val mfccSlid  = Sliding(mfcc, covSize, numCoef)
     val mfccSlidT = mfccSlid.drop(covSize)
     val el: Int   = (covSize / config.blockSize) + 1
-    val cov       = Pearson(mfccSlid.elastic(n = el), mfccSlidT, covSize)
+    val cov0      = Pearson(mfccSlid.elastic(n = el), mfccSlidT, covSize)
+    val cov       = cov0.take(numCov)
 
     val covF      = Frames(cov)
-    val numTop    = 1 // 10
-    val top10     = PriorityQueue( cov  , covF , size = numTop)  // best results mapped to frames
+    val top10     = PriorityQueue(-cov  , covF , size = numTop)  // lowest covariances mapped to frames
 //    val top10S    = PriorityQueue(-top10, top10, size = 10)  // frames in ascending order
 //    ResizeWindow(top10S, 1, 0, 1).poll(Metro(2), "frame") // XXX TODO -- we need a shortcut for this
 
     val top10Desc = PriorityQueue(top10, top10, size = numTop)  // frames in descending order
-    val frames    = numFrames +: (top10Desc * fftSize ) :+ 0L
+    // if we do _not_ add `sideLen`, we ensure the breaking change comes after the calculated frame
+    val frames    = numFrames +: ((top10Desc /* + sideLen */) * stepSize) :+ 0L
     val spans     = frames.tail zip frames
     ResizeWindow(spans, 1, 0, 1).poll(Metro(2), "frame") // XXX TODO -- we need a shortcut for this
 
