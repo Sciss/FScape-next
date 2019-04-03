23f9f171c677b04a39c105dd8ceb9b3be4a70488
Hanns Holger Rutz <contact@sciss.de>
2019-04-03 22:53:04 +0200

    this sh*t 's not ready

--- core/src/main/scala/de/sciss/fscape/graph/Gain.scala
+++ core/src/main/scala/de/sciss/fscape/graph/Gain.scala
@@ -1,52 +1,58 @@
-/*
- *  Gain.scala
- *  (FScape)
- *
- *  Copyright (c) 2001-2019 Hanns Holger Rutz. All rights reserved.
- *
- *  This software is published under the GNU Affero General Public License v3+
- *
- *
- *  For further information, please contact Hanns Holger Rutz at
- *  contact@sciss.de
- */
-
-package de.sciss.fscape.graph
-
-import de.sciss.fscape.UGenSource.unwrap
-import de.sciss.fscape.stream.{StreamIn, StreamOut}
-import de.sciss.fscape.{GE, UGen, UGenGraph, UGenIn, UGenInLike, UGenSource, stream}
-
-import scala.collection.immutable.{IndexedSeq => Vec}
-
-/** A utility UGen that can either change the relative gain of
-  * an input signal, or perform normalization (doing internal buffering in that case).
-  *
-  * For example, to boost the gain by 6 dB, one would use `mul = 6.0.dbAmp` and `normalized = 0`.
-  * To normalize the input with a 1 dB headroom, one would use `mul = -1.0.dbAmp` and `normalized = 1`.
-  *
-  * @param in           the signal to be adjusted in gain.
-  *                     Note that this UGen accepts a multi-channel input and will not apply
-  *                     multi-channel expansion, instead processing all channels together and
-  *                     outputting the adjusted multi-channel signal.
-  * @param mul          the gain factor, either a direct multiplier if `normalized` is zero,
-  *                     or the maximum amplitude if `normalized` is one.
-  * @param normalized   zero for relative gain adjustment, one to normalize the result.
-  * @param bipolar      used for normalization: whether the signal is bipolar (moving from
-  *                     `-maxAmp` to `+maxAmp` or unipolar (moving between zero and `+maxAmp`).
-  *                     For audio signals, this should be left at its default `1`, whereas it can
-  *                     be set to `0` for image data normalization.
-  */
-final case class Gain(in: GE, mul: GE = 1.0, normalized: GE = 1, bipolar: GE = 1)
-  extends UGenSource.MultiOut {
-
-  protected def makeUGens(implicit b: UGenGraph.Builder): UGenInLike =
-    unwrap(this, mul.expand +: normalized.expand +: bipolar.expand +: in.expand.outputs)
-
-  protected def makeUGen(args: Vec[UGenIn])(implicit b: UGenGraph.Builder): UGenInLike = {
-    val numOutputs = args.size - 3
-    UGen.MultiOut(this, args, numOutputs = numOutputs)
-  }
-
-  private[fscape] def makeStream(args: Vec[StreamIn])(implicit b: stream.Builder): Vec[StreamOut] = ???
-}
+///*
+// *  Gain.scala
+// *  (FScape)
+// *
+// *  Copyright (c) 2001-2019 Hanns Holger Rutz. All rights reserved.
+// *
+// *  This software is published under the GNU Affero General Public License v3+
+// *
+// *
+// *  For further information, please contact Hanns Holger Rutz at
+// *  contact@sciss.de
+// */
+//
+//package de.sciss.fscape.graph
+//
+//import de.sciss.fscape.UGenSource.unwrap
+//import de.sciss.fscape.stream.{StreamIn, StreamOut}
+//import de.sciss.fscape.{GE, UGen, UGenGraph, UGenIn, UGenInLike, UGenSource, stream}
+//
+//import scala.collection.immutable.{IndexedSeq => Vec}
+//
+///** A utility UGen that can either change the relative gain of
+//  * an input signal, or perform normalization (doing internal buffering in that case).
+//  *
+//  * For example, to boost the gain by 6 dB, one would use `mul = 6.0.dbAmp` and `normalized = 0`.
+//  * To normalize the input with a 1 dB headroom, one would use `mul = -1.0.dbAmp` and `normalized = 1`.
+//  *
+//  * @param in           the signal to be adjusted in gain.
+//  *                     Note that this UGen accepts a multi-channel input and will not apply
+//  *                     multi-channel expansion, instead processing all channels together and
+//  *                     outputting the adjusted multi-channel signal.
+//  * @param mul          the gain factor, either a direct multiplier if `normalized` is zero,
+//  *                     or the maximum amplitude if `normalized` is one.
+//  * @param normalized   zero for relative gain adjustment, one to normalize the result.
+//  * @param bipolar      used for normalization: whether the signal is bipolar (moving from
+//  *                     `-maxAmp` to `+maxAmp` or unipolar (moving between zero and `+maxAmp`).
+//  *                     For audio signals, this should be left at its default `1`, whereas it can
+//  *                     be set to `0` for image data normalization.
+//  * @param sync         `0` to have each channel treated independently, or `1` (default) to synchronise
+//  *                     the gain across channels.
+//  */
+//final case class Gain(in: GE, mul: GE = 1.0, normalized: GE = 1, bipolar: GE = 1, sync: GE = 1)
+//  extends UGenSource.MultiOut {
+//
+//  protected def makeUGens(implicit b: UGenGraph.Builder): UGenInLike =
+//    unwrap(this, mul.expand +: normalized.expand +: bipolar.expand +: sync.expand +: in.expand.outputs)
+//
+//  protected def makeUGen(args: Vec[UGenIn])(implicit b: UGenGraph.Builder): UGenInLike = {
+//    val numOutputs = args.size - 4
+//    UGen.MultiOut(this, args, numOutputs = numOutputs)
+//  }
+//
+//  private[fscape] def makeStream(args: Vec[StreamIn])(implicit b: stream.Builder): Vec[StreamOut] = {
+//    val mul +: normalized +: bipolar +: sync +: in = args
+//    stream.Gain(in = in.map(_.toDouble), mul = mul.toDouble, normalized = normalized.toInt,
+//      bipolar = bipolar.toInt, sync = sync.toInt)
+//  }
+//}
--- core/src/main/scala/de/sciss/fscape/stream/Gain.scala
+++ core/src/main/scala/de/sciss/fscape/stream/Gain.scala
@@ -1,302 +1,351 @@
-/*
- *  Gain.scala
- *  (FScape)
- *
- *  Copyright (c) 2001-2019 Hanns Holger Rutz. All rights reserved.
- *
- *  This software is published under the GNU Affero General Public License v3+
- *
- *
- *  For further information, please contact Hanns Holger Rutz at
- *  contact@sciss.de
- */
-
-package de.sciss.fscape
-package stream
-
-import akka.stream.stage.{InHandler, OutHandler}
-import akka.stream.{Attributes, Inlet, Outlet}
-import de.sciss.fscape.stream.impl.{NodeImpl, StageImpl}
-
-import scala.collection.immutable.{IndexedSeq => Vec, Seq => ISeq}
-
-object Gain {
-  def apply(in: ISeq[OutD], mul: OutD, normalized: OutI, bipolar: OutI)(implicit b: Builder): Vec[OutD] = {
-    val numChannels = in.size
-    val stage0      = new Stage(numChannels = numChannels, layer = b.layer)
-    val stage       = b.add(stage0)
-    b.connect(mul         , stage.inMul     )
-    b.connect(normalized  , stage.inNorm    )
-    b.connect(bipolar     , stage.inBipolar )
-
-    (in zip stage.ins).foreach { case (i, j) => b.connect(i, j) }
-
-    stage.outs
-  }
-
-  private final val name = "Gain"
-
-  private final case class Shape(inMul: InD, inNorm: InI, inBipolar: InI, ins: Vec[InD], outs: Vec[OutD])
-    extends akka.stream.Shape {
-
-    val inlets  : Vec[Inlet [_]] = inMul +: inNorm +: inBipolar +: ins
-    val outlets : Vec[Outlet[_]] = outs
-
-    def deepCopy(): Shape = {
-      Shape(inMul = inMul.carbonCopy(), inNorm = inNorm.carbonCopy(), inBipolar = inBipolar.carbonCopy(),
-        ins = ins.map(_.carbonCopy()), outs = outs.map(_.carbonCopy()))
-    }
-  }
-
-  private final class Stage(numChannels: Int, layer: Layer)(implicit ctrl: Control) extends StageImpl[Shape](name) {
-    val shape = Shape(
-      inMul     = InD(s"$name.mul"        ),
-      inNorm    = InI(s"$name.normalized" ),
-      inBipolar = InI(s"$name.bipolar"    ),
-      ins       = Vector.tabulate(numChannels)(ch => InD  (s"$name.in${ ch+1}")),
-      outs      = Vector.tabulate(numChannels)(ch => OutD (s"$name.out${ch+1}")),
-    )
-
-    def createLogic(attr: Attributes) = new Logic(shape, layer)
-  }
-
-  private final class Logic(shape: Shape, layer: Layer)(implicit ctrl: Control)
-    extends NodeImpl(name, layer, shape) { self =>
-
-    private[this] val numChannels = shape.ins.size
-
-    private[this] var mul       : Double  = _
-    private[this] var add       : Double  = _
-    private[this] var normalized: Boolean = _
-    private[this] var bipolar   : Boolean = _
-
-    private[this] var auxRemain = 3 // mul, normalized, bipolar
-    private[this] var auxReady  = false
-
-    // for normalization
-    private[this] var fileBuffers: Array[FileBuffer] = _
-    private[this] var minValue          = Double.PositiveInfinity
-    private[this] var maxValue          = Double.NegativeInfinity
-    private[this] var normFileBufReady  = false
-
-    private def decreaseAuxRemain(): Unit = {
-      auxRemain -= 1
-      if (auxRemain == 0) {
-        if (bipolar) {
-          fileBuffers = Array.fill(numChannels)(FileBuffer())
-          auxReady = true
-        }
-      }
-    }
-
-    override protected def stopped(): Unit = {
-      super.stopped()
-      if (fileBuffers != null) {
-        fileBuffers.foreach { fb =>
-          if (fb != null) fb.dispose()
-        }
-        fileBuffers = null
-      }
-    }
-
-    private class AuxHandler[A, E <: BufElem[A]](set: A => Unit)(in: Inlet[E]) extends InHandler {
-      private[this] var done  = false
-
-      override def toString: String = in.toString
-
-      def onPush(): Unit = {
-        logStream(s"onPush() $this")
-        val b = grab(in)
-        if (!done && b.size > 0) {
-          val v = b.buf(0)
-          done  = true
-          set(v)
-          decreaseAuxRemain()
-        }
-        b.release()
-      }
-
-      override def onUpstreamFinish(): Unit = {
-        logStream(s"onUpstreamFinish() $this")
-        if (!done) super.onUpstreamFinish()
-      }
-    }
-
-    private class MainInHandler(ch: Int) extends InHandler {
-      override def toString: String = shape.ins(ch).toString
-
-      def onPush(): Unit = {
-        logStream(s"onPush() $this")
-        inPushed(ch)
-      }
-
-      override def onUpstreamFinish(): Unit = {
-        logStream(s"onUpstreamFinish() $this")
-        inClosed(ch)
-      }
-    }
-
-    private class MainOutHandler(ch: Int) extends OutHandler {
-      override def toString: String = shape.outs(ch).toString
-
-      def onPull(): Unit = {
-        logStream(s"onPull() $this")
-        outPulled(ch)
-      }
-
-      override def onDownstreamFinish(): Unit = {
-        logStream(s"onDownstreamFinish() $this")
-        ??? // outClosed(ch)
-      }
-    }
-
-    setHandler(shape.inMul    , new AuxHandler((v: Double)  => mul        = v     )(shape.inMul    ))
-    setHandler(shape.inNorm   , new AuxHandler((v: Int)     => normalized = v != 0)(shape.inNorm   ))
-    setHandler(shape.inBipolar, new AuxHandler((v: Int)     => bipolar    = v != 0)(shape.inBipolar))
-
-    for (ch <- 0 until numChannels) {
-      setHandler(shape.ins  (ch), new MainInHandler (ch))
-      setHandler(shape.outs (ch), new MainOutHandler(ch))
-    }
-
-    //////////////
-
-    private def analyzeNorm(b: BufD): Unit = {
-      val buf   = b.buf
-      var i     = 0
-      val sz    = b.size
-      var _min  = minValue
-      var _max  = maxValue
-      while (i < sz) {
-        val v = buf(i)
-        if (v < _min) _min = v
-        if (v > _max) _max = v
-        i += 1
-      }
-      minValue = _min
-      maxValue = _max
-    }
-
-    private def inPushed(ch: Int): Unit = if (auxReady) {
-      val in = shape.ins(ch)
-      if (normalized) {
-        val b   = grab(in)
-        val fb  = fileBuffers(ch)
-        fb.write(b.buf, 0, b.size)
-        analyzeNorm(b)
-        b.release()
-        if (isClosed(in)) {
-          checkNormInputsDone()
-
-        } else {
-          pull(in)
-        }
-
-      } else {
-        val out = shape.outs(ch)
-        if (isAvailable(out)) {
-          pipeDirect(in, out)
-        }
-      }
-    }
-
-    private def checkNormInputsDone(): Unit =
-      if (shape.ins.forall(in => isClosed(in) && !isAvailable(in))) {
-        normFileBufReady = true
-        fileBuffers.foreach(_.rewind())
-        add   = if (bipolar) 0.0 else -minValue
-        mul   = if (bipolar) {
-          val ceil = math.max(math.abs(minValue), math.abs(minValue))
-          if (ceil > 0.0) mul/ceil else 0.0
-
-        } else {
-          val span = maxValue - minValue
-          if (span > 0.0) mul/span else 0.0
-        }
-        checkPipeNorm()
-      }
-
-    private def checkPipeNorm(): Unit = {
-      var numClosed = 0
-      var numDone   = 0
-      var ch = 0
-      while (ch < numChannels) {
-        val out     = shape.outs(ch)
-        if (isClosed(out)) {
-          numClosed += 1
-        } else if (isAvailable(out)) {
-          val fb = fileBuffers(ch)
-          if (fb == null) {
-            numDone += 1
-          } else {
-            val done = pipeNorm(fileBuffers(ch), out)
-            if (done) {
-              fb.dispose()
-              fileBuffers(ch) = null
-              numDone += 1
-            }
-          }
-        }
-        ch += 1
-      }
-
-      if (numClosed == numChannels || numDone == numChannels) completeStage()
-    }
-
-    private def pipeNorm(fileBuf: FileBuffer, out: OutD): Boolean = {
-      val rem = fileBuf.numFrames - fileBuf.position
-      if (rem == 0L) return true
-
-      val b     = ctrl.borrowBufD()
-      val chunk = math.min(rem, b.size).toInt
-      b.size    = chunk
-      fileBuf.read(b.buf, 0, chunk)
-      if (add != 0.0) Util.add(b.buf, 0, chunk, add)
-      if (mul != 1.0) Util.mul(b.buf, 0, chunk, mul)
-      push(out, b)
-      false
-    }
-
-    private def outPulled(ch: Int): Unit = if (auxReady) {
-      val out = shape.outs(ch)
-      if (normalized) {
-        if (normFileBufReady) {
-          checkPipeNorm()
-        }
-
-      } else {
-        val in = shape.ins(ch)
-        if (isAvailable(in)) {
-          pipeDirect(in, out)
-        }
-      }
-    }
-
-    private def pipeDirect(in: InD, out: OutD): Unit = {
-      val b = grab(in)
-      Util.mul(b.buf, 0, b.size, mul)
-      push(out, b)
-      tryPull(in)
-    }
-
-    private def inClosed(ch: Int): Unit = {
-      val in = shape.ins(ch)
-      if (isAvailable(in)) {
-        inPushed(ch)
-
-      } else {
-        if (auxReady) {
-          ???
-          if (normalized) {
-
-          } else {
-
-          }
-
-        } else {
-          completeStage()
-        }
-      }
-    }
-  }
-}
+///*
+// *  Gain.scala
+// *  (FScape)
+// *
+// *  Copyright (c) 2001-2019 Hanns Holger Rutz. All rights reserved.
+// *
+// *  This software is published under the GNU Affero General Public License v3+
+// *
+// *
+// *  For further information, please contact Hanns Holger Rutz at
+// *  contact@sciss.de
+// */
+//
+//package de.sciss.fscape
+//package stream
+//
+//import akka.stream.stage.{InHandler, OutHandler}
+//import akka.stream.{Attributes, Inlet, Outlet}
+//import de.sciss.fscape.stream.impl.{NodeImpl, StageImpl}
+//
+//import scala.collection.immutable.{IndexedSeq => Vec, Seq => ISeq}
+//
+//object Gain {
+//  def apply(in: ISeq[OutD], mul: OutD, normalized: OutI, bipolar: OutI, sync: OutI)(implicit b: Builder): Vec[OutD] = {
+//    val numChannels = in.size
+//    val stage0      = new Stage(numChannels = numChannels, layer = b.layer)
+//    val stage       = b.add(stage0)
+//    b.connect(mul         , stage.inMul     )
+//    b.connect(normalized  , stage.inNorm    )
+//    b.connect(bipolar     , stage.inBipolar )
+//    b.connect(sync        , stage.inSync    )
+//
+//    (in zip stage.ins).foreach { case (i, j) => b.connect(i, j) }
+//
+//    stage.outs
+//  }
+//
+//  private final val name = "Gain"
+//
+//  private final case class Shape(inMul: InD, inNorm: InI, inBipolar: InI, inSync: InI, ins: Vec[InD], outs: Vec[OutD])
+//    extends akka.stream.Shape {
+//
+//    val inlets  : Vec[Inlet [_]] = inMul +: inNorm +: inBipolar +: inSync +: ins
+//    val outlets : Vec[Outlet[_]] = outs
+//
+//    def deepCopy(): Shape = {
+//      Shape(inMul = inMul.carbonCopy(), inNorm = inNorm.carbonCopy(), inBipolar = inBipolar.carbonCopy(),
+//        inSync = inSync.carbonCopy(), ins = ins.map(_.carbonCopy()), outs = outs.map(_.carbonCopy()))
+//    }
+//  }
+//
+//  private final class Stage(numChannels: Int, layer: Layer)(implicit ctrl: Control) extends StageImpl[Shape](name) {
+//    val shape = Shape(
+//      inMul     = InD(s"$name.mul"        ),
+//      inNorm    = InI(s"$name.normalized" ),
+//      inBipolar = InI(s"$name.bipolar"    ),
+//      inSync    = InI(s"$name.sync"       ),
+//      ins       = Vector.tabulate(numChannels)(ch => InD  (s"$name.in${ ch+1}")),
+//      outs      = Vector.tabulate(numChannels)(ch => OutD (s"$name.out${ch+1}")),
+//    )
+//
+//    def createLogic(attr: Attributes) = new Logic(shape, layer)
+//  }
+//
+//  private final class Logic(shape: Shape, layer: Layer)(implicit ctrl: Control)
+//    extends NodeImpl(name, layer, shape) { self =>
+//
+//    private[this] val numChannels = shape.ins.size
+//
+//    private[this] var mul       : Double  = _
+//    private[this] var add       : Double  = _
+//    private[this] var normalized: Boolean = _
+//    private[this] var bipolar   : Boolean = _
+//    private[this] var sync      : Boolean = _
+//    private[this] val channels  = Array.tabulate(numChannels)(new MainChannel(_))
+//
+//    private[this] var auxRemain = 4 // mul, normalized, bipolar, sync
+//    private[this] var auxReady  = false
+//
+//    // clean up
+//
+//    override protected def stopped(): Unit = {
+//      super.stopped()
+//      channels.foreach(_.stopped())
+//    }
+//
+//    // aux handling
+//
+//    private def decreaseAuxRemain(): Unit = {
+//      auxRemain -= 1
+//      if (auxRemain == 0) {
+//        auxReady = true
+//        channels.foreach(_.notifyAuxReady())
+//      }
+//    }
+//
+//    private class AuxHandler[A, E <: BufElem[A]](set: A => Unit)(in: Inlet[E]) extends InHandler {
+//      private[this] var done  = false
+//
+//      override def toString: String = in.toString
+//
+//      def onPush(): Unit = {
+//        logStream(s"onPush() $this")
+//        val b = grab(in)
+//        if (!done && b.size > 0) {
+//          val v = b.buf(0)
+//          done  = true
+//          set(v)
+//          decreaseAuxRemain()
+//        }
+//        b.release()
+//      }
+//
+//      override def onUpstreamFinish(): Unit = {
+//        logStream(s"onUpstreamFinish() $this")
+//        if (!done) super.onUpstreamFinish()
+//      }
+//
+//      setHandler(in, this)
+//    }
+//
+//    // input / output
+//
+//    private class MainChannel(ch: Int) extends InHandler with OutHandler {
+//      override def toString: String = s"$self channel $ch" // shape.ins(ch).toString
+//
+//      private[this] var fileBuf: FileBuffer = _
+//
+//      private[this] val in  = shape.ins (ch)
+//      private[this] val out = shape.outs(ch)
+//
+//      var done = false
+//
+//      var minValue: Double = Double.PositiveInfinity
+//      var maxValue: Double = Double.NegativeInfinity
+//
+//      var normFileBufReady = false
+//
+//      private[this] var minMaxValid = false
+//
+//      def stopped(): Unit =
+//        disposeNoFire()
+//
+//      private def disposeNoFire(): Unit =
+//        if (fileBuf != null) {
+//          fileBuf.dispose()
+//          fileBuf = null
+//        }
+//
+//      private def dispose(): Unit = {
+//        done = true
+//        disposeNoFire()
+//        checkAllClosed()
+//      }
+//
+//      def onPush(): Unit = {
+//        logStream(s"onPush() $this")
+//        inPushed()
+//      }
+//
+//      override def onUpstreamFinish(): Unit = {
+//        logStream(s"onUpstreamFinish() $this")
+//        inClosed()
+//      }
+//
+//      def onPull(): Unit = {
+//        logStream(s"onPull() $this")
+//        outPulled()
+//      }
+//
+//      override def onDownstreamFinish(): Unit = {
+//        logStream(s"onDownstreamFinish() $this")
+//        outClosed()
+//      }
+//
+//      private def inPushed(): Unit = if (auxReady) {
+//        if (normalized) {
+//          assert (!normFileBufReady)
+//          val b = grab(in)
+//          fileBuf.write(b.buf, 0, b.size)
+//          analyzeNorm(b)
+//          b.release()
+//          if (isClosed(in)) {
+//            inClosed()  // may start rewinding file-buf etc.
+//          } else {
+//            pull(in)
+//          }
+//
+//        } else {
+//          if (isAvailable(out)) {
+//            pipeDirect()
+//          }
+//        }
+//      }
+//
+//      private def analyzeNorm(b: BufD): Unit = {
+//        val buf   = b.buf
+//        var i     = 0
+//        val sz    = b.size
+//        var _min  = minValue
+//        var _max  = maxValue
+//        while (i < sz) {
+//          val v = buf(i)
+//          if (v < _min) _min = v
+//          if (v > _max) _max = v
+//          i += 1
+//        }
+//        minValue = _min
+//        maxValue = _max
+//      }
+//
+//      private def inClosed(): Unit = {
+//        if (isAvailable(in)) {
+//          inPushed()
+//
+//        } else {
+//          if (auxReady && normalized) {
+//            if (!normFileBufReady) {
+//              normFileBufReady = true
+//              fileBuf.rewind()
+//              if (sync) {
+//                syncCheckAllFileBuffersReady()
+//              } else {
+//                notifyMinMaxValid()
+//              }
+//            }
+//          } else {
+//            done = true
+//            checkAllClosed()
+//          }
+//        }
+//      }
+//
+//      def notifyMinMaxValid(): Unit = {
+//        minMaxValid = true
+//        add   = if (bipolar) 0.0 else -minValue
+//        mul   = if (bipolar) {
+//          val ceil = math.max(math.abs(minValue), math.abs(minValue))
+//          if (ceil > 0.0) mul/ceil else 0.0
+//
+//        } else {
+//          val span = maxValue - minValue
+//          if (span > 0.0) mul/span else 0.0
+//        }
+//        checkPipeNorm()
+//      }
+//
+//      def notifyAuxReady(): Unit = {
+//        if (isAvailable(in  )) inPushed()
+//        if (isAvailable(out )) outPulled()
+//      }
+//
+//      private def checkPipeNorm(): Unit = {
+//        if (isAvailable(out) && !done) {
+//          done = pipeNorm()
+//          if (done) dispose()
+//        }
+//      }
+//
+//      private def outPulled(): Unit = if (auxReady) {
+//        if (normalized) {
+//          if (minMaxValid) {
+//            checkPipeNorm()
+//          }
+//
+//        } else {
+//          if (isAvailable(in)) {
+//            pipeDirect()
+//          }
+//        }
+//      }
+//
+//      private def outClosed(): Unit =
+//        dispose()
+//
+//      private def pipeNorm(): Boolean = {
+//        val rem = fileBuf.numFrames - fileBuf.position
+//        if (rem == 0L) return true
+//
+//        val b     = ctrl.borrowBufD()
+//        val chunk = math.min(rem, b.size).toInt
+//        b.size    = chunk
+//        fileBuf.read(b.buf, 0, chunk)
+//        if (add != 0.0) Util.add(b.buf, 0, chunk, add)
+//        if (mul != 1.0) Util.mul(b.buf, 0, chunk, mul)
+//        push(out, b)
+//        false
+//      }
+//
+//      private def pipeDirect(): Unit = {
+//        val b = grab(in)
+//        Util.mul(b.buf, 0, b.size, mul)
+//        push(out, b)
+//        tryPull(in)
+//      }
+//
+//      setHandler(in , this)
+//      setHandler(out, this)
+//    }
+//
+//    // setup
+//
+//    new AuxHandler((v: Double)  => mul        = v     )(shape.inMul     )
+//    new AuxHandler((v: Int)     => normalized = v != 0)(shape.inNorm    )
+//    new AuxHandler((v: Int)     => bipolar    = v != 0)(shape.inBipolar )
+//    new AuxHandler((v: Int)     => sync       = v != 0)(shape.inSync    )
+//
+//    // ...
+//
+//    private def checkAllClosed(): Unit = {
+//      val numClosed = channels.count(_.done)
+//      if (numClosed == numChannels) completeStage()
+//    }
+//
+//    private def syncCheckAllFileBuffersReady(): Unit = {
+//      assert (sync)
+//
+//      var numClosed = 0
+//      var numReady  = 0
+//
+//      channels.foreach { h =>
+//        if (h.done) {
+//          numClosed += 1
+//          numReady  += 1
+//        } else if (h.normFileBufReady) {
+//          numReady  += 1
+//        }
+//      }
+//
+//      if      (numClosed == numChannels) completeStage()
+//      else if (numReady  == numChannels) {
+//
+//        var allMin = Double.PositiveInfinity
+//        var allMax = Double.NegativeInfinity
+//
+//        channels.foreach { h =>
+//          allMin  = math.min(allMin, h.minValue)
+//          allMax  = math.max(allMax, h.maxValue)
+//        }
+//
+//        channels.foreach { h =>
+//          h.minValue  = allMin
+//          h.maxValue  = allMax
+//          if (!h.done) {
+//            h.notifyMinMaxValid()
+//          }
+//        }
+//      }
+//    }
+//  }
+//}
