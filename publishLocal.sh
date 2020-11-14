70ce6f5db23aca6879f32ae12c193ada2747374d
Hanns Holger Rutz <contact@sciss.de>
2020-11-14 16:40:11 +0100

    ...

---
+++ publishLocal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sbt +coreJS/publishLocal +coreJVM/publishLocal +fscape-cdp/publishLocal +fscape-macros/publishLocal +fscape-modules/publishLocal +fscape-views/publishLocal +lucreJS/publishLocal +lucreJVM/publishLocal
+
